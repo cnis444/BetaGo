156cd7c0be98399cd2927199333c52dede5160cf
cnis <cedric.nis@dbmail.com>
2020-01-15 17:13:49 +0100

    go finish

--- go.py
+++ go.py
@@ -17,13 +17,16 @@ class Board(object):
         self.n = n
         self.round = 0
         self.MOVE = [(1,0), (0,1), (-1,0), (0,-1)]
+        self.toCheck = []
+
 
     
     def play(self, move):
         if move == (-1,-1):
             self.passRound += 1
             if self.passRound == 2:
-                raise Exception("OUI!!!! la partie est finie")
+                score = self.calculatePoint()
+                raise Exception("OUI!!!! la partie est finie : " + str(score))
             self.nextPlayer *=-1
             self.round +=1
             return
@@ -36,7 +39,6 @@ class Board(object):
 
         self.value[move[0]+1, move[1]+1] = self.nextPlayer;    
         self.degre[move] = 0
-
         for x,y in self.MOVE:
             if(self.value[move[0]+x+1, move[1]+y+1] == self.EMPTY):
                 self.degre[move] += 1
@@ -56,7 +58,8 @@ class Board(object):
 
         self.round +=1
         if self.round == self.maxturn:
-            raise Exception("Vous avez vraiment joué si longtemps ? --'")
+            score = self.calculatePoint()
+            raise Exception("Vous avez vraiment joué si longtemps ? --' : " + str(score))
         
         self.nextPlayer *=-1
 
@@ -70,24 +73,22 @@ class Board(object):
 
 
     def destroy(self, coord):
-        print("distroille : ", coord)
         self.parent[coord] = [-1,-1]
         self.degre[coord] = 0
+        tmp = self.value[coord[0]+1, coord[1]+1]
         self.value[coord[0]+1, coord[1]+1] = self.EMPTY
         for x,y in self.MOVE:
-            if(self.value[coord[0]+x+1, coord[1]+y+1] == self.value[coord[0]+1, coord[1]+1] * -1):
-                self.degre[self.getParent((move[0]+x, move[1]+y))] += 1
+            if(self.value[coord[0]+x+1, coord[1]+y+1] == tmp * -1):
+                self.degre[self.getParent((coord[0]+x, coord[1]+y))] += 1
                 
-            elif(self.value[coord[0]+x+1, coord[1]+y+1] == self.value[coord[0]+1, coord[1]+1]):
-                self.destroy((move[0]+x, move[1]+y))
+            elif(self.value[coord[0]+x+1, coord[1]+y+1] == tmp):
+                self.destroy((coord[0]+x, coord[1]+y))
 
 
     def getParent(self, coord):
-        print("recur : ", coord)
         if self.parent[coord][0] == -1 and self.parent[coord][1] == -1:
             return coord
         parcoord = self.getParent((self.parent[coord][0], self.parent[coord][1]))
-        print("testparent ", self.parent[coord], " ", parcoord)
         self.parent[coord][0] = parcoord[0]
         self.parent[coord][1] = parcoord[1]
         return parcoord
@@ -96,6 +97,54 @@ class Board(object):
     def merge(self, coord1, coord2):
         self.degre[self.getParent(coord2)] += self.degre[self.getParent(coord1)]
         self.parent[self.getParent(coord1)] = self.getParent(coord2) 
+
+
+    def explore(self, type, coord):
+        #print(coord , type, self.toCheck)
+        self.toCheck.remove(coord)
+        toret = [type, 1]
+        for x,y in self.MOVE:
+            #print(coord, x, y)
+            val = self.value[coord[0]+x+1, coord[1]+y+1]
+            if val == self.EMPTY and (coord[0]+x, coord[1]+y) in self.toCheck:
+                res = self.explore(toret[0], (coord[0]+x, coord[1]+y))
+                if toret[0] is None:
+                    toret[0] = res[0]
+                elif toret[0] is not None and toret[0] != res[0]:
+                    toret[0] = 0
+                toret[1] += res[1]
+            elif val != self.EMPTY:
+                if val == self.BORDER:
+                    continue
+                if toret[0] is None:
+                    toret[0] = val
+                elif toret[0] is not None and val != toret[0]:
+                    toret[0] = 0
+            #print(coord, toret)
+        return toret
+
+
+
+    def calculatePoint(self):
+        point = [0,0]
+        for x in range(self.n):
+            for y in range(self.n):
+                val = self.value[x+1,y+1]
+                if val == self.EMPTY:
+                    self.toCheck.append((x,y))
+                elif val == self.BLACK:
+                    point[0] +=1
+                elif val == self.WHITE:
+                    point[1] += 1
+        while(len(self.toCheck) > 0):
+            pos = self.toCheck[0]
+            res = self.explore(None, pos)
+            if res[0] == self.BLACK:
+                point[0] += res[1]
+            elif res[0] == self.WHITE:
+                point[1] += res[1]
+        return point
+
         
 
     def clone(self):
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 
 from go import Board
 
-b = Board(5)
+b = Board(19)
 
 while 1:
-    print(b.value,"\n",b.parent,"\n", b.degre)
+    print(b.value)
     x = int(input("x:"))
     y = int(input("y:"))
     b.play((x,y))
+
